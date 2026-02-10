bf8c683b3d9052137cc822e0424bd59a24d61628
tfladager <170553495+tfladager@users.noreply.github.com>
2026-02-10 16:33:57 -0700

    Cowboy Stuff

---
+++ HW-4/script.js
@@ -0,0 +1,4 @@
+let name = document.getElementById("rider-name").value;
+document.getElementById("story-text").innerText = "Nice to meet you, " 
+document.getElementById("horse-stories").style.display = "block";
+document.getElementById("rider-selection").style.display = "none";  
