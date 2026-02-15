3eb5564ad840d9d9bd7806759e84d5a4110d3578
tfladager <170553495+tfladager@users.noreply.github.com>
2026-02-15 12:39:04 -0700

    try again

--- HW-5/script.js
+++ HW-5/script.js
@@ -31,7 +31,8 @@ function handleFirstChoice() {
     if (choice === "arena") {
         document.getElementById("story-text").innerText =
             "You head to the arena. Before the horses can start running the barrels, they have to be warmed up with loops around the arena and varying speeds. We will start at a walk, then move to a lope.";
-        document.getElementById("arena-choice").style.display = "block";    
+        document.getElementById("arena-choice").style.display = "none";
+        document.getElementById("arena-choice").style.display = "none";    
     } else if (choice === "pasture") {
         document.getElementById("story-text").innerText =
             "You ride out to the pasture. Our ride starts by checking the fence and water tanks. We will enjoy the day as we go.";
